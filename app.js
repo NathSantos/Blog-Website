204dd864c43b11f506d97dc22a19234492e93b82
Nathália Santos <63311872+NathSantos@users.noreply.github.com>
2023-04-10 06:24:08 -0400

    adding connection to mongoDB

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const bodyParser = require("body-parser");
 // Uma das funcionalidades do Lodash é ignorar caracteres maiúsculos ou minúsculos e se tem caracteres especiais entre a palavra.
 // Usado em urls para ter equidade nos valores passados.
 const lodash = require('lodash');
+const mongoose = require('mongoose');
 
 const homeTextContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutTextContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactTextContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const listComposes = [];
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser:true});
+
+const postsSchema = {
+  title: String,
+  content: String
+}
+
+const Post = mongoose.model("Post", postsSchema); 
 
 const app = express();
 
@@ -17,7 +27,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-  res.render('home', {homeContent: homeTextContent, listOfComposes: listComposes});
+  Post.find().then((posts) => {
+    res.render('home', {homeContent: homeTextContent, listOfComposes: posts});
+  });
 });
 
 app.get('/about', function(req, res) {
@@ -34,25 +46,21 @@ app.get('/compose', function(req, res) {
 
 app.post('/compose', function(req, res) {
 
-  const postCompose = {
+  const postCompose = new Post({
     title: req.body.composeTitle,
-    text: req.body.composeText
-  }
+    content: req.body.composeText
+  });
 
-  listComposes.push(postCompose);
+  postCompose.save();
 
   res.redirect('/');
 });
 
-app.get('/posts/:item', function(req, res) {
-  const urlItem = lodash.lowerCase(req.params.item);
+app.get('/posts/:postId', function(req, res) {
+  const requestedPostId = req.params.postId;
 
-  listComposes.forEach(function(item) {
-    const currentItem = lodash.lowerCase(item.title);
-
-    if(currentItem == urlItem) {
-      res.render('post', {postTitle: item.title, postContent:item.text});
-    } 
+  Post.findOne({_id: requestedPostId}).then((post) => {
+    res.render('post', {postTitle: post.title, postContent:post.content});
   });
 });
 
